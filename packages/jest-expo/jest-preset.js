938472d17bd785c1f14649229ba7f464b7a21fb6
James Ide <ide@expo.io>
2019-04-21 13:56:09 -0700

    [sdk33] Fix jest-preset so it uses the correct assetFileTransformer path (fixes Home tests)
    The path to the assetFileTransformer wasn't right, causing Home tests to fail. Now Home tests pass.

--- packages/jest-expo/jest-preset.js
+++ packages/jest-expo/jest-preset.js
@@ -19,7 +19,7 @@ if (!jestPreset.transform[defaultAssetNamePattern]) {
 
 const assetNamePattern =
   '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp|ttf|otf|m4v|mov|mp4|mpeg|mpg|webm|aac|aiff|caf|m4a|mp3|wav|html|pdf|obj)$';
-jestPreset.transform[assetNamePattern] = 'jest-expo/src/assetFileTransformer.js';
+jestPreset.transform[assetNamePattern] = 'jest-expo/src/preset/assetFileTransformer.js';
 
 // transformIgnorePatterns
 if (!Array.isArray(jestPreset.transformIgnorePatterns)) {
